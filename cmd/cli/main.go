eef4cb7e391648e6a9a1b2a753157171ec0bc31c
BasedAlex <alexbroseph@mail.ru>
2024-03-28 21:39:15 +0400

    feat:first-review-fixes

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -4,33 +4,54 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"slices"
 	"strings"
+	"unicode"
 
 	"github.com/kljensen/snowball"
+	"github.com/kljensen/snowball/english"
 )
 
 
 func main() {
 	var str string
 
-	flag.StringVar(&str, "str", "follower brings bunch of questions follow", "input a string to be stemmed")
+	flag.StringVar(&str, "s", "follower brings bunch of questions follow", "input a string to be stemmed")
 	flag.Parse()
+	
+	seen := make(map[string]int)
+
+	f := func(c rune) bool {
+		return !unicode.IsLetter(c) && !unicode.IsNumber(c)
+	}
 
 	words := strings.Split(str, " ")
-	filterMap := dictionary()
-	res := make([]string, 0)
+	var res string
 
-	for i := 0; i < len(words); i++ {
-		stemmed, err := snowball.Stem(words[i], "english", true)
+	for _, word := range words {
+		if english.IsStopWord(word) {
+			continue
+		}
+
+		stemmed, err := snowball.Stem(word, "english", true)
 		if err != nil {
 			log.Fatal(err)
 		}
-		if !filterMap[stemmed] {
-			res = append(res, stemmed)
+		fmt.Println(stemmed)
+
+		// we don't need any word that includes apostrophes
+		if slices.Contains([]byte(stemmed), 39) {
+			continue
 		}
-		filterMap[stemmed] = true
+
+		if seen[stemmed] == 0 {
+			seen[stemmed]++
+			res = res + " " + stemmed
+		}
+
 	}
 
-	fmt.Println(strings.Join(res, " ")) 
+
+	fmt.Println(strings.Join(strings.FieldsFunc(res, f), " ")) 
 }
 
